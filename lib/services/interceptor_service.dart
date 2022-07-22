60afbeba80978566fae5fa7d18d0cd2006b92c43
Nasibali <abdiyevnasibali@gmail.com>
2022-07-22 19:33:41 +0500

    Update

--- lib/services/interceptor_service.dart
+++ lib/services/interceptor_service.dart
@@ -5,18 +5,19 @@ import 'package:my_dog_app/services/log_service.dart';
 class InterceptorService implements InterceptorContract {
   @override
   Future<RequestData> interceptRequest({required RequestData data}) async {
-    LogService.i(data.url.toString());
-    LogService.i(data.method.toString());
-    LogService.i(data.headers.toString());
-    LogService.i(data.params.toString());
-    LogService.i(data.body.toString());
+    debugPrint(data.url.toString());
+    // LogService.i(data.url.toString());
+    // LogService.i(data.method.toString());
+    // LogService.i(data.headers.toString());
+    // LogService.i(data.params.toString());
+    // LogService.i(data.body.toString());
     return data;
   }
 
   @override
   Future<ResponseData> interceptResponse({required ResponseData data}) async {
-    LogService.v(data.statusCode.toString());
-    LogService.wtf(data.body.toString());
+    // LogService.v(data.statusCode.toString());
+    // LogService.wtf(data.body.toString());
     return data;
   }
 }
